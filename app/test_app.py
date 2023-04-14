ecd80c10b480ad4045babf5d3a2035e42ab7e8ff
saminaqazi123456 <33048021+saminaqazi123456@users.noreply.github.com>
2023-04-14 13:56:01 +0000

    updated microservice

--- app/test_app.py
+++ app/test_app.py
@@ -1,3 +1,35 @@
+# import pytest
+
+# from run import app as application
+
+
+# @pytest.fixture()
+# def app():
+#     application.config.update({
+#         "TESTING": True,
+#     })
+#     yield application
+
+
+# @pytest.fixture
+# def client(app):   
+#     return app.test_client()
+
+
+# @pytest.fixture()
+# def runner(app):
+#     return app.test_cli_runner()
+
+
+# def test_api(client):
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert b"Flask" in response.data
+
+
+# Latest code below
+
+
 import pytest
 
 from run import app as application
@@ -22,6 +54,17 @@ def runner(app):
 
 
 def test_api(client):
-    response = client.get("/")
+    response = client.get("/api/v1/movies/recommend")
     assert response.status_code == 200
-    assert b"Flask" in response.data
+    assert response.is_json
+    assert response.get_json()[0]["title"] != ""
+
+    response = client.get("/api/v1/movies/recommend?title=Kingpin")
+    assert response.status_code == 200
+    assert response.is_json
+    assert len(response.get_json()) >= 2
+
+    response = client.get("/api/v1/movies/recommend?title=Lost%20in%20Translation")
+    assert response.status_code == 200
+    assert response.is_json
+    assert len(response.get_json()) >= 5
